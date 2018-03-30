37e58f51854805a1b69b461656edc93e43a695de
Ltrmex <maciekmajchrzak23@wp.pl>
2018-03-30 20:32:26 +0100

    fixed compile errors, created stacks and map of special characters

--- Shunt.go
+++ Shunt.go
@@ -1,30 +1,37 @@
 package main
 
-import {
+//	Imports
+import (
 	"fmt"
-}	//	import
+)	//	import
 
+//	Convert infix to pofix
 func intopost(infix string) string {
-	postfix := "";
-
-	return postfix;
+	//	Map special characters (e.g. *) stored as integers
+	specials := map[rune]int{'*':10, '.':9, '|': 8}	//	keeping track of precedence of special characters
+	
+	//	Array of runes of empty
+	pofix := []rune{}	//	runes - character as displayed on the screen
+	s: = []rune{}	//	stack of operators from infix of regular expression
+	
+	return string(pofix)
 }	//	intopost()
 
 func main() {
 	//	Answer: ab.c*.
-	fmt.Println("Infix: ", "a.b.c");
-	fmt.Println("Postfix: ", intopost("a.b.c"));
+	fmt.Println("Infix: ", "a.b.c*")
+	fmt.Println("Postfix: ", intopost("a.b.c*"))
 
 	//	Answer: abd|.*
-	fmt.Println("Infix: ", "(a.(b|d))*");
-	fmt.Println("Postfix: ", intopost("(a.(b|d))*"));
+	fmt.Println("Infix: ", "(a.(b|d))*")
+	fmt.Println("Postfix: ", intopost("(a.(b|d))*"))
 
 	//	Answer: abd|.c*.
-	fmt.Println("Infix: ", "(a.(b|d)).c*");
-	fmt.Println("Postfix: ", intopost("(a.(b|d)).c*"));
+	fmt.Println("Infix: ", "(a.(b|d)).c*")
+	fmt.Println("Postfix: ", intopost("(a.(b|d)).c*"))
 
 	//	Answer: abb.+.c.
-	fmt.Println("Infix: ", "a.(b.b)+.c");
-	fmt.Println("Postfix: ", intopost("a.(b.b)+.c"));
+	fmt.Println("Infix: ", "a.(b.b)+.c")
+	fmt.Println("Postfix: ", intopost("a.(b.b)+.c"))
 
 }	//	main()

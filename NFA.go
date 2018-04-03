f3c8b64e77520eae2f15370e27bf5c11908fd46b
Ltrmex <maciekmajchrzak23@wp.pl>
2018-04-03 20:03:39 +0100

    created required structs

--- NFA.go
+++ NFA.go
@@ -5,11 +5,29 @@ import (
 	"fmt"
 )	//	import
 
-func poregtonfa(pofix string) {
+//  Max arrows from a state is two
+type state struct {
+    symbol rune //  instead of labeling the arrows
+
+    //  Pointers to other states
+    edge1 *state    //  arrow one
+    edge2 *state    //  arrow two
+}   //  state
+
+//  Thompson's construction - for each fragment there is one initial state and one accept state
+//  Keep track of initial state and accept state of fragment of nfa
+type nfa struct {
+    initial *state
+    accept *state
+}   //  nfa
+
+//  Postfix regular expression to non deterministic finite automaton
+func ConvertToNFA(pofix string) *nfa {
 
 }   //  poregtonfa()
 
+//  Main
 func main() {
-    nfa := poregtonfa("ab.c*|")
+    nfa := ConvertToNFA("ab.c*|")
     fmt.Println(nfa)
 }   //  main()

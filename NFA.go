f56725093874d4c157cbe670074bd3b0a4e0c9af
Ltrmex <maciekmajchrzak23@wp.pl>
2018-04-03 19:49:21 +0100

    setup for NFA - Thompson's construction

---
+++ NFA.go
@@ -0,0 +1,15 @@
+package main
+
+//	Imports
+import (
+	"fmt"
+)	//	import
+
+func poregtonfa(pofix string) {
+
+}   //  poregtonfa()
+
+func main() {
+    nfa := poregtonfa("ab.c*|")
+    fmt.Println(nfa)
+}   //  main()
